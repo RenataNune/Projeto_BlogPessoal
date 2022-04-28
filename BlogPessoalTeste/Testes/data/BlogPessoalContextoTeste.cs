4c39d7196285f7f04979ea4995c4c0fd8d1eb0ca
RenataNune <renatan.sil97@gmail.com>
2022-04-28 16:59:24 -0300

    @RenataNune [Task:Teste de Contexto]{28/04/2022 16:57}

---
+++ BlogPessoalTeste/Testes/data/BlogPessoalContextoTeste.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using BlogPessoal.src.data;
+using BlogPessoal.src.modelos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BlogPessoalTeste.Testes.data
+{
+    [TestClass]
+    public class BlogPessoalContextoTeste
+    {
+        private BlogPessoalContexto _contexto;
+
+        [TestInitialize]
+        public void Inicio()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+                .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+                .Options;
+
+            _contexto = new BlogPessoalContexto(opt);
+        }
+
+        [TestMethod]
+        public void InserirNovoUsuarioNoBancoRetornaUsuario()
+        {
+            Usuariomodelo usuario = new Usuariomodelo();
+
+            usuario.Nome = "Renata nuens";
+            usuario.Email = "renatan.sil97@gmail.com";
+            usuario.Senha = "123456";
+            usuario.Foto = "aquitaolinkdafoto";
+
+            _contexto.Usuarios.Add(usuario);
+
+            _contexto.SaveChanges();
+
+
+            Assert.IsNotNull(_contexto.Usuarios.FirstOrDefault(u => u.Email == "renatan.sil97@gmail.com"));
+
+        }
+    }
+}
